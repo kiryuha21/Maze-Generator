26c65f4a9c53aed192d03d3aa12f7fd8c77596dc
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2024-02-06 18:30:38 +0300

    tests updated

--- src/tests/route_search_tests.cc
+++ src/tests/route_search_tests.cc
@@ -9,30 +9,29 @@ const std::string kCasesPath = "tests/routing cases/";
 
 TEST(route_search_tests, single_cell_test) {
   RouteModel router(1, 1, 1, 1, kCasesPath + "one_cell.mp");
-
   auto expected = std::vector<std::pair<int, int>>{{1, 1}};
+
   ASSERT_EQ(router.find_route(), expected);
 }
 
 TEST(route_search_tests, no_way_test) {
   RouteModel router(1, 1, 2, 2, kCasesPath + "no_way.mp");
 
-  auto expected = std::vector<std::pair<int, int>>{};
-  ASSERT_EQ(router.find_route(), expected);
+  ASSERT_THROW(router.find_route(), std::logic_error);
 }
 
 TEST(route_search_tests, single_way_test) {
   RouteModel router(1, 1, 4, 4, kCasesPath + "single_way.mp");
-
   auto expected = std::vector<std::pair<int, int>>{
       {4, 4}, {3, 4}, {3, 3}, {4, 3}, {4, 2}, {4, 1}, {3, 1}, {2, 1}, {1, 1}};
+
   ASSERT_EQ(router.find_route(), expected);
 }
 
 TEST(route_search_tests, multiple_way_test) {
   RouteModel router(1, 1, 1, 3, kCasesPath + "multiple_ways.mp");
-
   auto expected = std::vector<std::pair<int, int>>{{1, 3}, {1, 2}, {1, 1}};
+
   ASSERT_EQ(router.find_route(), expected);
 }
 }  // namespace s21
